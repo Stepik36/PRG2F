ff3718b2be6f36955c128e44e59d83cfb2ad04ec
stepan.zezula <stepan.zezula@infis.cz>
2023-11-02 12:46:08 +0100

    pole

--- src/cvika/LoopsPractice.java
+++ src/cvika/LoopsPractice.java
@@ -4,30 +4,31 @@ import java.util.Scanner;
 
 public class LoopsPractice {
     public static void main(String[] args) {
-//        System.out.println("zadej cisla, zadavani ukoncis zadanim -1");
+        System.out.println("zadej cisla, zadavani ukoncis zadanim -1");
         Scanner sc = new Scanner(System.in);
         int input;
-//        int input = sc.nextInt();
-//        while (input != -1){
-//            input = sc.nextInt();
-//            if (input % 2 == 0){
-//                System.err.println(input);
-//            }
-//            input = sc.nextInt();
-//        }
-//
-//        System.out.println("zadej maximum");
-//        for (int i = 0;i < input; i++){
-//            if (i % 2 == 0){
-//                System.out.println(i);
-//            }
-//        }
-        System.out.println("zadej cislo");
-        input = sc.nextInt();
-        for (int i = 2; i < input; i++) {
-            if (input % i == 0) {
+        int i = sc.nextInt();
+        while (i != -1) {
+            input = sc.nextInt();
+            if (input % 2 == 0) {
+                System.err.println(input);
+            }
+            input = sc.nextInt();
+        }
 
+        System.out.println("zadej maximum");
+        for (int i = 0; i < i; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
             }
         }
     }
-}
+//       System.out.println("zadej cislo");
+//        input = sc.nextInt();
+//        for (int i = 2; i < input; i++) {
+//            if (input % i == 0) {
+
+//            }
+//        }
+//    }
+//}
---
+++ src/pole/Arrays.java
@@ -0,0 +1,4 @@
+package pole;
+
+public class Arrays {
+}
