15aa3531ec0ae962a55b206b73ed93605ce54d7d
stepan.zezula <stepan.zezula@infis.cz>
2023-11-02 12:57:22 +0100

    pole

--- src/pole/Arrays.java
+++ src/pole/Arrays.java
@@ -1,4 +1,12 @@
 package pole;
 
 public class Arrays {
+    public static void main(String[] args) {
+        int[] pole = {5 , 32 ,21 ,15};
+        int[] pole2 = new int[10];
+        System.out.println(pole[2]);
+        for (int i = 0; i < pole.length; i++) {
+            System.out.println(pole[i]);
+        }
+    }
 }
