ea07b4abedd023fac6eae5f81b5298ef71c803c5
stepan.zezula <stepan.zezula@infis.cz>
2023-11-23 13:03:52 +0100

    pow

---
+++ src/pole/Convert.java
@@ -0,0 +1,15 @@
+package pole;
+
+public class Convert {
+    public static void main(String[] args) {
+        int[] binaryArray = {1,0,1,1,1,1,0,1};
+        int result = 0;
+        for (int i = 0; i < binaryArray.length; i++) {
+            result += binaryArray[i]* (int)Math.pow(2,i);
+
+
+        }
+        System.out.println(result);
+        ;
+    }
+}
